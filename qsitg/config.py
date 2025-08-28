7a3067e8d2bc045f873b30a7dcc337837a041788
olivierdalang <olivier.dalang@gmail.com>
2025-08-28 16:02:49 +0200

    fix: raster and thematic urls

--- qsitg/config.py
+++ qsitg/config.py
@@ -1,15 +1,15 @@
 AUTH_SETTING_ID = "sitgOA2"
 ARCGISFEATURESERVERS = {
     "vector.sitg.ge.ch": {
-        "url": "https://vector.sitg.ge.ch/arcgis/rest/services/Hosted",
+        "url": "https://vector.sitg.ge.ch/arcgis/rest/services/Hosted/",
         "authcfg": AUTH_SETTING_ID,
     },
     "raster.sitg.ge.ch": {
-        "url": "https://raster.sitg.ge.ch/arcgis/rest/services/Hosted",
+        "url": "https://raster.sitg.ge.ch/arcgis/rest/services/",
         "authcfg": AUTH_SETTING_ID,
     },
     "thematic.sitg.ge.ch": {
-        "url": "https://thematic.sitg.ge.ch/arcgis/rest/services/Hosted",
+        "url": "https://thematic.sitg.ge.ch/arcgis/rest/services/",
         "authcfg": AUTH_SETTING_ID,
     },
 }
